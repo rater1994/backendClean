ebbae5c5d4efa6e8d8b7b1af962143bf83eb8025
Mark <athauf94@gmail>
2018-11-02 10:09:10 +0200

    Last test for gitignore

--- src/main/java/com/server/car/Repository/CarRepository.java
+++ src/main/java/com/server/car/Repository/CarRepository.java
@@ -4,6 +4,8 @@ import com.server.car.Entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//Get JPA
+
 @Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
 
